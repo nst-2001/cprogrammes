91250f3be2d72cc46b185f96270b0ee4447c459a
Navya Susan Thomas <62889118+riyon-susan@users.noreply.github.com>
2020-03-31 20:34:56 +0530

    first commit

---
+++ assignment2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+ void main()
+{
+    printf("Navya Susan Thomas");
+    printf("\nElectronics & Communications");
+    printf("\nCollege of Engineering,Chengannur");
+    return 0;
+}
