b6dfdc79ea55974f852b2ba133d68922c1c5cfbc
Navya Susan Thomas <62889118+riyon-susan@users.noreply.github.com>
2020-04-01 20:22:37 +0530

    second commit

---
+++ sam3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+main()
+{
+    char str[6] ="NAVYA";
+    char school[6] ="KEEMS";
+    int age = 18;
+    float marks = 85.0;
+    int class = 12;
+    printf("NAME: %s\n",str);
+    printf("SCHOOL:%s\n",school);
+    printf("AGE:%d\n", age);
+    printf("CLASS:%d\n",class);
+    printf("MARKS:%1f",marks);
+    return 0;
+}
---
+++ sam4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+    char name[] = "Brooney";
+    int age = 1;
+    char favfood[] = " Pedigree,Cerelac" ;
+    printf("Name:%s\n", name);
+    printf("Age: %d\n", age);
+    printf("Favourite Food: %s", favfood);
+    return 0;
+}
